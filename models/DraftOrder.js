d9bf26248826635892d6219a69129a6073c70f1b
keshavsni <keshav.sni96@gmail.com>
2022-06-24 18:31:30 +0530

    draftorder

--- models/DraftOrder.js
+++ models/DraftOrder.js
@@ -71,4 +71,13 @@ const draftOrderSchema = new mongoose.Schema({
     }
 })
 
+draftOrderSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+       
+    }
+})
+
 module.exports = mongoose.model('draft_orders',draftOrderSchema)
