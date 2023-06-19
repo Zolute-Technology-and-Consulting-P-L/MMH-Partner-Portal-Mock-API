edba7fbf8c70ff2776b140f4941610989e4a818e
Maruti Raj <mraj317@gmail.com>
2023-06-19 18:15:37 +0530

    with mobile

--- controllers/PartnerController.js
+++ controllers/PartnerController.js
@@ -2,7 +2,7 @@ const PartnersModel = require("../models/PartnersModel");
 const auth = require("../middlewares/jwt");
 
 const verifyOtp = async ({ mobile, otp }) => {
-  let user = await PartnersModel.findOne({ mobile: +mobile }).exec();
+  let user = await PartnersModel.findOne({ mobile, otp }).exec();
   console.log("User:", user, mobile, otp);
   if (!user) {
     return null;
